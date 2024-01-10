5295376728998f2a30ae096d0b7c7de13fd188bb
SanishKumar <18sanishkumar@gmail.com>
2024-01-10 21:28:14 +0530

    Restarting to get on track (57:46)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center px-4">
       
     </main>
-  )
+  );
 }
